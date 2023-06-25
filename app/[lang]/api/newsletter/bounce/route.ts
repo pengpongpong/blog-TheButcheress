0e4c0e034a24255e164546ee6b2c2e02f709ae91
Phuong T <t.m.phuong@hotmail.com>
2023-06-26 01:00:19 +0200

    added bounce to update database

--- app/[lang]/api/newsletter/bounce/route.ts
+++ app/[lang]/api/newsletter/bounce/route.ts
@@ -1,7 +1,23 @@
+import { connectToDatabase } from "@/components/utils/db";
+import EmailModel from "@/models/EmailModel";
+import { String } from "aws-sdk/clients/appstream";
 import { NextRequest, NextResponse } from "next/server";
+
 
 export const POST = async (req: NextRequest) => {
     const data = await req.json()
+
+    await connectToDatabase()
+
+    data.mail.destination.map((recipient: string) => {
+        const getEmail = async (email: String) => {
+            await EmailModel.findOneAndUpdate({ email: email }, { active: "blocked", error: "bounce" })
+        }
+        getEmail(recipient)
+    })
+
+
+
 
     return NextResponse.json({ message: JSON.stringify(data) }, { status: 201 })
 }
--- models/EmailModel.ts
+++ models/EmailModel.ts
@@ -5,7 +5,8 @@ mongoose.Promise = global.Promise;
 const EmailSchema = new Schema({
     email: { type: String, required: true },
     active: { type: String, default: "pending", required: true },
-    date: { type: Date, default: Date.now, required: true }
+    error: { type: String },
+    date: { type: Date, default: Date.now, required: true },
 })
 
 export default mongoose.models.Emails || mongoose.model("Emails", EmailSchema)
