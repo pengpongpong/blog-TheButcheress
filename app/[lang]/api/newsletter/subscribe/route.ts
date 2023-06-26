67ccc0fc85843afe56d07d5f69f89a1bea482954
Phuong T <t.m.phuong@hotmail.com>
2023-06-26 03:22:29 +0200

    update subscripe api

--- app/[lang]/api/newsletter/subscribe/route.ts
+++ app/[lang]/api/newsletter/subscribe/route.ts
@@ -8,8 +8,8 @@ const ses = new aws.SES({
     // apiVersion: "2010-12-01",
     region: "eu-central-1",
     credentials: {
-        accessKeyId: process.env.SES_AWS_ACCESS_KEY,
-        secretAccessKey: process.env.SES_AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
+        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
     }
 });
 
