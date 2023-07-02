1b639c7d271dad04f446df1e614fe6d6d6e43234
Phuong T <t.m.phuong@hotmail.com>
2023-07-03 00:58:30 +0200

    next-auth callback

--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -12,7 +12,7 @@ const SignIn = ({ callbackUrl }: { callbackUrl: string }) => {
     return (
         <main className="flex flex-col justify-center items-center gap-8">
             <h1 className="text-6xl text-center font-text">Nicht eingeloggt!</h1>
-            <Link className="btn btn-primary"  href={`${process.env.NEXT_PUBLIC_DOMAIN}/de/api/auth/signin?callbackUrl=${process.env.NEXT_PUBLIC_DOMAIN + callbackUrl}`}>LOGIN</Link>
+            <Link className="btn btn-primary" href={`${process.env.NEXT_PUBLIC_DOMAIN}/de/api/auth/signin?callbackUrl=${callbackUrl}`}>LOGIN</Link>
         </main>
     )
 }
