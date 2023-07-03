7f78af97d71aa77b2db746fb12746dfcb13b0da2
Phuong T <t.m.phuong@hotmail.com>
2023-07-03 16:23:50 +0200

    change auth options

--- app/[lang]/api/auth/[...nextauth]/route.ts
+++ app/[lang]/api/auth/[...nextauth]/route.ts
@@ -36,18 +36,18 @@ export const authOption = {
             }
         })
     ],
-    baseUrl: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+    // baseUrl: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    // secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
     debug: true,
-    callbacks: {
-        async redirect({ url, baseUrl }: {url: string, baseUrl: string}) {
-            // Allows relative callback URLs
-            if (url.startsWith("/")) return `${baseUrl}${url}`
-            // Allows callback URLs on the same origin
-            else if (new URL(url).origin === baseUrl) return url
-            return baseUrl
-        }
-    }
+    // callbacks: {
+    //     async redirect({ url, baseUrl }: {url: string, baseUrl: string}) {
+    //         // Allows relative callback URLs
+    //         if (url.startsWith("/")) return `${baseUrl}${url}`
+    //         // Allows callback URLs on the same origin
+    //         else if (new URL(url).origin === baseUrl) return url
+    //         return baseUrl
+    //     }
+    // }
 }
 
 const handler = NextAuth(authOption)
